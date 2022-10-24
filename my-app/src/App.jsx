17406f04caa8df42af4913a26f2da3da0300ea11
Master <blinkerion@gmail.com>
2022-10-24 14:33:31 -0500

    quality of life changes

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,11 +2,19 @@ import React from "react"
 import Header from "./Header"
 import Footer from "./Footer"
 import Note from "./Note"
+import notes from "./notes.js"
+
 
 function App() {
     return <div>
         <Header/>
-        <Note/>
+        <dl>{notes.map( newNote => 
+     ( <Note
+            key={newNote.key}
+            title={newNote.title}
+            content={newNote.content}
+        />)
+)}</dl>
         <Footer/>
     </div>
 }
