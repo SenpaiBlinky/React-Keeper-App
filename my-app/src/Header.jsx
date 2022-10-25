f70ecf93565e3984426fb4a16809909aac58ed63
Master <blinkerion@gmail.com>
2022-10-25 18:04:26 -0500

    quality of life changes

--- my-app/src/Header.jsx
+++ my-app/src/Header.jsx
@@ -1,8 +1,20 @@
-import React from "react"
+import React, {useState} from "react"
 
 function Header() {
+
+    // messing with reast state
+    let [count, setCount] = useState(0)
+
+function increase() {
+    setCount(count += 1)
+} // end of messing
+
 return <header>
 <h1>Keeper</h1>
+
+{/* messing with react state */}
+<p>{count}</p> 
+<button onClick={increase}>+</button>
 </header>
 
 }
