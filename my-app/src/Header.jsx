2aea124fb1d0b7b8edec87d27bf0d78fd55dd6f5
Master <blinkerion@gmail.com>
2022-10-25 18:18:02 -0500

    adding react hooks(useState)

--- my-app/src/Header.jsx
+++ my-app/src/Header.jsx
@@ -5,16 +5,38 @@ function Header() {
     // messing with reast state
     let [count, setCount] = useState(0)
 
+function decrease() {
+    setCount(count -= 1)
+}
+
 function increase() {
     setCount(count += 1)
 } // end of messing
+
+
+
+//  --------------------------------------TIME DISPLAY---------------------------------------------
+
+
+let [time, updateTime] = useState(new Date().getTime)
+
+function getTime() {
+    updateTime(new Date().getTime)
+}
+
 
 return <header>
 <h1>Keeper</h1>
 
 {/* messing with react state */}
 <p>{count}</p> 
-<button onClick={increase}>+</button>
+<button onClick={increase}>increase</button>
+<button onClick={decrease}>decrease</button>
+
+<p>{time}</p> 
+<button onClick={getTime}>increase</button>
+
+<p>{currentTime}</p> 
 </header>
 
 }
